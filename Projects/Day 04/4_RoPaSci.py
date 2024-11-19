706448dc89fdf30aa79b559831b6fc34e4a4daa4
Tauqeer Darve <91264028+tauqeer-darve@users.noreply.github.com>
2024-11-19 10:01:00 +0530

    Update 4_RoPaSci.py

--- Projects/Day 04/4_RoPaSci.py
+++ Projects/Day 04/4_RoPaSci.py
@@ -1,4 +1,6 @@
 import random
+import os
+
 rock = '''
     _______
 ---'   ____)
@@ -28,24 +30,38 @@ scissors = '''
 ---.__(___)
 SCISSORS
 '''
+def clear_screen():
+    # For Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # For macOS and Linux
+    else:
+        os.system('clear')
 
-rps = [rock,paper,scissors]
-player_choice=int(input("Rock, Paper or Scissors?(Type 0 for Rock, Type 1 for Paper or Type 2 for Scissors):\n"))
-game_choice=random.randint(0,2)
-print(f"You chose: {rps[player_choice]}")
-print(f"Computer chose: {rps[game_choice]}")
-if player_choice >= 3 or player_choice < 0:
-    print("Invalid Option. YOU LOSE!")
-elif player_choice == 0 and game_choice == 2:
-    print("You win!")
-elif game_choice == 0 and player_choice == 2:
-    print("You lose!")
-elif player_choice < game_choice:
-    print("You lose!")
-elif game_choice < player_choice:
-    print("You Win!")
-elif player_choice == game_choice:
-    print("It's a darw!")
-else:
-    print("Invalid Input!")
-
+def rps():
+    rps = [rock,paper,scissors]
+    player_choice=int(input("Rock, Paper or Scissors?(Type 0 for Rock, Type 1 for Paper or Type 2 for Scissors):\n"))
+    game_choice=random.randint(0,2)
+    print(f"You chose: {rps[player_choice]}")
+    print(f"Computer chose: {rps[game_choice]}")
+    if player_choice >= 3 or player_choice < 0:
+        print("Invalid Option. YOU LOSE!")
+    elif player_choice == 0 and game_choice == 2:
+        print("You win!")
+    elif game_choice == 0 and player_choice == 2:
+        print("You lose!")
+    elif player_choice < game_choice:
+        print("You lose!")
+    elif game_choice < player_choice:
+        print("You Win!")
+    elif player_choice == game_choice:
+        print("It's a draw!")
+    else:
+        print("Invalid Input!")
+        
+        
+rps()
+while input("Do you want to play again?(y/n): ").lower() == "y":
+    clear_screen()
+    rps()
+    
