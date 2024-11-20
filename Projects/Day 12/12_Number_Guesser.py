9a2ec4c7df4c0310608150af19d60264f8a25871
Tauqeer Darve <91264028+tauqeer-darve@users.noreply.github.com>
2024-11-20 16:59:03 +0530

    Update 12_Number_Guesser.py

--- Projects/Day 12/12_Number_Guesser.py
+++ Projects/Day 12/12_Number_Guesser.py
@@ -35,7 +35,7 @@ def diff(chances):
 
 def num_checker(number,guess):
     if number == guess:
-        print(f"You got it! The answer was {number}.\n")
+        print(f"\nYou got it! The answer was {number}.\n")
         return True
     elif number > guess:
         print("Too low")
@@ -53,15 +53,15 @@ def guess_the_number():
     tries = diff(chances)
     while not game_over:
         if tries > 1:
-            print(f"You have {tries} attempts remaining to guess the number.")
+            print(f"\nYou have {tries} attempts remaining to guess the number.")
         else:
-            print(f"You have {tries} attempt remaining to guess the number.")
+            print(f"\nYou have {tries} attempt remaining to guess the number.")
         tries -= 1
         guess = int(input("Make a guess: "))
         if num_checker(number,guess):
             game_over = True
         elif tries == 0:
-            print(f"You've run out of guesses. The number was {number}.\n")
+            print(f"\nYou've run out of guesses. The number was {number}.\n")
             game_over = True
 
 guess_the_number()
