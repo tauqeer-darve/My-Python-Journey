fd8a166d974e5a3014a4117e4fac4ad4d618a750
Tauqeer Darve <darvetauqeer1@gmail.com>
2025-01-04 19:50:00 +0530

    Progress Update...

--- Projects/Day 16/OOP-Coffee-Machine/main.py
+++ Projects/Day 16/OOP-Coffee-Machine/main.py
@@ -19,6 +19,3 @@ while is_on:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
-
-
-
